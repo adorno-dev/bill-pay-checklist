943b6feed53d5fb0f33db9e4c725d20e1fa71216
adorno-dev <adorno.dev@gmail.com>
2023-09-24 20:40:02 -0300

    Forms Validation

--- src/BillPayChecklist.MVC/ViewModels/BillViewModel.cs
+++ src/BillPayChecklist.MVC/ViewModels/BillViewModel.cs
@@ -1,10 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace BillPayChecklist.MVC.ViewModels
 {
     public class BillViewModel
     {
         public Guid Id { get; set; }
+        [Required]
         public string? Title { get; set; }
+        [Required]
         public int DueDay { get; set; }
+        [Required]
         public int RefMonth { get; set; }
         public bool Recurrent { get; set; }
 
--- src/BillPayChecklist.MVC/ViewModels/PaymentViewModel.cs
+++ src/BillPayChecklist.MVC/ViewModels/PaymentViewModel.cs
@@ -1,13 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace BillPayChecklist.MVC.ViewModels
 {
     public class PaymentViewModel
     {
         public Guid Id { get; set; }
+        [Required]
         public DateOnly PaymentDate { get; set; }
+        [Required]
         public int RefMonth { get; set; }
+        [Required]
         public decimal? Amount { get; set; }     
         public string? Comment { get; set; }
 
+        [Required]
         public Guid BillId { get; set; }
         public BillViewModel? Bill { get; set; }
     }
